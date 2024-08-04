37d4138523f5db2f5ca6cfd780c711b1f1939249
mkaur711 <mkaur711@uw.edu>
2024-08-04 01:20:14 -0600

    Time: 10 ms (29.98%) | Memory: 9.4 MB (55.64%) - LeetSync

--- Leetcode75/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ Leetcode75/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,30 +1,62 @@
+// class Solution {
+// public:
+//     bool isVowel(char s){
+//         s=tolower(s);
+//         if(s=='a' ||s=='e'||s=='i'||s=='o'||s=='u'){
+//             return true;
+//         }
+//         else
+//         return false;
+//     }
+//     string reverseVowels(string s) {
+//         int l=0;
+//         int r= s.length()-1;
+//         while(l<r){
+//             if(!isVowel(s[r])){
+//                 r--;
+//             }
+//             if(!isVowel(s[l])){
+//                 l++;
+//             }
+//             if(isVowel(s[l]) && isVowel(s[r])){
+//                 swap(s[l],s[r]);
+//                 l++;
+//                 r--;
+//             }
+            
+//         }
+//         return s;
+//     }
+// };
+
 class Solution {
 public:
-    bool isVowel(char s){
-        s=tolower(s);
-        if(s=='a' ||s=='e'||s=='i'||s=='o'||s=='u'){
-            return true;
-        }
-        else
-        return false;
+    bool isVowel(char s) {
+        s = tolower(s);  // Convert character to lowercase to handle both cases
+        return (s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u');
     }
+
     string reverseVowels(string s) {
-        int l=0;
-        int r= s.length()-1;
-        while(l<r){
-            if(!isVowel(s[r])){
-                r--;
-            }
-            if(!isVowel(s[l])){
+        int l = 0;
+        int r = s.length() - 1;  // Correct length
+
+        while (l < r) {
+            // Move left pointer to the next vowel
+            while (l < r && !isVowel(s[l])) {
                 l++;
             }
-            if(isVowel(s[l]) && isVowel(s[r])){
-                swap(s[l],s[r]);
+            // Move right pointer to the previous vowel
+            while (l < r && !isVowel(s[r])) {
+                r--;
+            }
+            // Swap vowels
+            if (l < r) {
+                swap(s[l], s[r]);
                 l++;
                 r--;
             }
-            
         }
+
         return s;
     }
 };
