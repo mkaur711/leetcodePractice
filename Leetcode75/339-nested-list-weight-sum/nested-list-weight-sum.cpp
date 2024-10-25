3dd513418ee0e0a9ec245ef7b35f8e9f8637d1c3
mkaur711 <mkaur711@uw.edu>
2024-10-25 09:27:34 -0700

    Time: 0 ms (100.00%) | Memory: 11.6 MB (50.76%) - LeetSync

---
+++ Leetcode75/339-nested-list-weight-sum/nested-list-weight-sum.cpp
@@ -0,0 +1,58 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    int depthSum(vector<NestedInteger>& nestedList) {
+         queue<vector<NestedInteger>> q;
+        q.push(nestedList);
+
+        int sum = 0;
+        int level = 1;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i=0; i<size; ++i) {
+                vector<NestedInteger> p = q.front();
+                q.pop();
+
+                for (int i=0; i<p.size(); ++i) {
+                    if (p[i].isInteger()) {
+                        sum += p[i].getInteger() * level;
+                    } else {
+                        q.push(p[i].getList());
+                    }
+                } 
+            }
+            level++;
+        }
+        
+        return sum;
+    }
+    
+};
