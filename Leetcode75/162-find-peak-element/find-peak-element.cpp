90f4081dfe269175a64070f993a06d92a519a92b
mkaur711 <mkaur711@uw.edu>
2024-11-02 14:19:12 -0700

    Time: 0 ms (100.00%) | Memory: 11.6 MB (63.92%) - LeetSync

--- Leetcode75/162-find-peak-element/find-peak-element.cpp
+++ Leetcode75/162-find-peak-element/find-peak-element.cpp
@@ -1,20 +1,27 @@
+#include <vector>
+#include <limits>
+
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int left = 0, right = nums.size() - 1;
-        
+    int findPeakElement(std::vector<int>& nums) {
+        int left = 0;
+        int right = nums.size();
+
         while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > nums[mid + 1]) {
-                // Peak is in the left half (including mid)
-                right = mid;
-            } else {
-                // Peak is in the right half (excluding mid)
+            int mid = (left + right) / 2;
+            int mid_val = nums[mid];
+            int to_left = (mid > 0) ? nums[mid - 1] : std::numeric_limits<int>::min();
+            int to_right = (mid < nums.size() - 1) ? nums[mid + 1] : std::numeric_limits<int>::min();
+
+            if (to_left < mid_val && mid_val > to_right) {
+                return mid;
+            } else if (to_left < mid_val && mid_val < to_right) {
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        
         return left;
     }
 };
+
