04b0ba442e9225a9069f703d3a5aed6fe8069210
mkaur711 <mkaur711@uw.edu>
2024-11-12 20:47:07 -0800

    Time: 0 ms (100.00%) | Memory: 63.3 MB (68.14%) - LeetSync

--- Leetcode75/975-range-sum-of-bst/range-sum-of-bst.cpp
+++ Leetcode75/975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -11,19 +11,17 @@
  */
 class Solution {
 public:
-    int rangeSumBST(TreeNode* root, int low, int high) {
-        if(root==nullptr) return 0;
-        int sum=0;
-        if(root->val>=low && root->val<=high)
-        {
-            sum+=root->val;
+    int rangeSumBST(TreeNode* root,int low,int high){
+        if(!root) return 0;
+        if(root->val>=low && root->val<=high){
+            return root->val+rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
+
         }
-        if(root->val >low){
-            sum+=rangeSumBST(root->left,low,high);
+        else if(root->val<low){
+            return rangeSumBST(root->right,low,high);
         }
-        if(root->val<high){
-            sum+=rangeSumBST(root->right,low,high);
+        else{
+            return rangeSumBST(root->left,low,high);
         }
-        return sum;
     }
 };
