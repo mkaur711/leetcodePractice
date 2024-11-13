8eefa9266522f7cc829bba578850f2089d402f1d
mkaur711 <mkaur711@uw.edu>
2024-11-12 19:01:36 -0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (31.58%) - LeetSync

--- Leetcode75/408-valid-word-abbreviation/valid-word-abbreviation.cpp
+++ Leetcode75/408-valid-word-abbreviation/valid-word-abbreviation.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    bool validWordAbbreviation(string word, string abbr) {
-    int i = 0, j = 0; // i for word, j for abbr
-
-    while (i < word.length() && j < abbr.length()) {
-        if (isalpha(abbr[j])) {
-            // If the character in abbreviation is a letter, it should match the word's character
-            if (word[i] != abbr[j]) {
-                return false;
-            }
-            i++;
-            j++;
-        } else if (isdigit(abbr[j])) {
-            // If it's a digit, calculate the number of characters to skip
-            if (abbr[j] == '0') {
-                return false; // leading zeros are not allowed
-            }
-            int num = 0;
-            while (j < abbr.length() && isdigit(abbr[j])) {
-                num = num * 10 + (abbr[j] - '0');
+    bool validWordAbbreviation(string word,string abbr){
+        int i=0,j=0; 
+        while(i<word.length()&&j<abbr.length()){
+            if(isdigit(abbr[j])){
+                int start=j;
+                while(j<abbr.length() &&isdigit(abbr[j]))
+                {j++;}
+                int num=stoi(abbr.substr(start,j-start));
+                if(num==0 || (abbr[start] == '0' && j - start > 1)) return false;
+                i+=num;
+                }
+            else{
+                if(word[i]!=abbr[j]) return false;
+                i++;
                 j++;
             }
-            i += num; // Skip the characters in word
         }
-    }
-
-    // Check if both word and abbreviation have been completely traversed
-    return i == word.length() && j == abbr.length();
+        return i==word.length()&&j==abbr.length();
     }
 };
