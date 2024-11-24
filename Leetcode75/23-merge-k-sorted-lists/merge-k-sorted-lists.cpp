13ac523240b7e83aacf0297d4dc606ce5ca7a66a
mkaur711 <mkaur711@uw.edu>
2024-11-24 15:45:12 -0800

    Time: 0 ms (100.00%) | Memory: 18.4 MB (50.69%) - LeetSync

--- Leetcode75/23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ Leetcode75/23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -11,39 +11,47 @@
 #include <queue>
 
 
-class Compare {
-public:
-    bool operator() (ListNode* a, ListNode* b) {
-        return a->val > b->val; // Min-heap based on node value
-    }
-};
+
 class Solution {
 public:
-    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
-        std::priority_queue<ListNode*, std::vector<ListNode*>, Compare> minHeap;
+    // Compare function to be used by priority queue (min-heap)
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val; // Compare based on value to maintain min-heap property
+        }
+    };
 
-        // Initialize the min-heap with the head of each list
+    // Main function to merge k sorted linked-lists
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // Priority Queue to get the smallest element from the k lists
+        priority_queue<ListNode*, vector<ListNode*>, Compare> minHeap;
+
+        // Push the first element of each list into the min-heap
         for (ListNode* list : lists) {
             if (list) {
                 minHeap.push(list);
             }
         }
 
-        ListNode dummy(0);
-        ListNode* current = &dummy;
+        ListNode *head = new ListNode(0); // Dummy node to simplify the result list creation
+        ListNode *current = head;
 
-        // Merge the lists
+        // While there are elements in the min-heap
         while (!minHeap.empty()) {
-            ListNode* smallestNode = minHeap.top();
+            // Get the smallest element from the heap
+            ListNode* node = minHeap.top();
             minHeap.pop();
-            current->next = smallestNode; // Attach the smallest node
-            current = current->next; // Move the pointer forward
 
-            if (smallestNode->next) {
-                minHeap.push(smallestNode->next); // Push the next node into the heap
+            // Add this element to the merged list
+            current->next = node;
+            current = current->next;
+
+            // If the current list has more elements, push the next one to the heap
+            if (node->next) {
+                minHeap.push(node->next);
             }
         }
 
-        return dummy.next; // Return the merged linked list
-    }
-};
+        // Return the merged list, skipping the dummy head node
+        return head->next;
+    }};
