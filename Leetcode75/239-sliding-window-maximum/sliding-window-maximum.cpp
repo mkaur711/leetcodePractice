3c07ef9064037e31e2a5037c83996de1c9a5a86e
mkaur711 <mkaur711@uw.edu>
2024-11-24 11:38:46 -0800

    Time: 39 ms (41.21%) | Memory: 137.4 MB (67.46%) - LeetSync

--- Leetcode75/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ Leetcode75/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> res;
-        deque<int> deque;
+        deque<int> dq; // To store indices of elements in the current window
+    vector<int> result; // To store the maximums for each window
 
-        for (int idx = 0; idx < nums.size(); idx++) {
-            int num = nums[idx];
-
-            while (!deque.empty() && deque.back() < num) {
-                deque.pop_back();
-            }
-            deque.push_back(num);
-
-            if (idx >= k && nums[idx - k] == deque.front()) {
-                deque.pop_front();
-            }
-
-            if (idx >= k - 1) {
-                res.push_back(deque.front());
-            }
+    for (int i = 0; i < nums.size(); i++) {
+        // Remove indices that are out of the current window
+        if (!dq.empty() && dq.front() < i - k + 1) {
+            dq.pop_front();
         }
 
-        return res;        
+        // Remove elements from the back that are smaller than the current element
+        while (!dq.empty() && nums[dq.back()] < nums[i]) {
+            dq.pop_back();
+        }
+
+        // Add the current element's index
+        dq.push_back(i);
+
+        // Add the maximum to the result (start adding after the first k elements)
+        if (i >= k - 1) {
+            result.push_back(nums[dq.front()]);
+        }
+    }
+
+    return result;     
     }
 };
